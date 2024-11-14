07e5d4fc946486a0358d5cd91db7eca44ed2e923
VydehiR <vydehi1303@gmail.com>
2024-11-14 17:30:36 -0600

    changes made

--- controllers/Sculptures.js
+++ controllers/Sculptures.js
@@ -49,12 +49,6 @@ exports.Sculptures_create_post = async function(req, res) {
   }
 };
 
-const results = [
-    { Sculptures_name: "The Thinker", Sculptures_height: "182", Sculptures_material: "Bronze" },
-    { Sculptures_name: "Venus de Milo", Sculptures_height: "203", Sculptures_material: "Marble" },
-    { Sculptures_name: "David", Sculptures_height: "517", Sculptures_material: "Marble" }
-  ];
-
   // Handle Sculpture delete on DELETE
 exports.Sculptures_delete = async function(req, res) {
   try {
