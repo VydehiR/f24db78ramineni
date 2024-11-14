628bde686635f49a508930d847d7aa90e55c8da7
VydehiR <vydehi1303@gmail.com>
2024-11-14 17:27:35 -0600

    changes made

--- models/sculptures.js
+++ models/sculptures.js
@@ -6,10 +6,6 @@ const SculpturesSchema = new mongoose.Schema({
   size: { type: String, required: true },
   cost: { type: Number, required: true }
 });
-const results = [
-    { Sculptures_name: "The Thinker", Sculptures_height: "182", Sculptures_material: "Bronze" },
-    { Sculptures_name: "Venus de Milo", Sculptures_height: "203", Sculptures_material: "Marble" },
-    { Sculptures_name: "David", Sculptures_height: "517", Sculptures_material: "Marble" }
-  ];
+
 // Export the model
 module.exports = mongoose.model('sculptures', SculpturesSchema);
