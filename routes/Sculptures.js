9ac266789c293a876ea53f5ed3b6f5a2ea2b2cc7
VydehiR <vydehi1303@gmail.com>
2024-11-20 17:50:11 -0600

    create delete details update filesa are created.

--- routes/Sculptures.js
+++ routes/Sculptures.js
@@ -2,11 +2,21 @@ var express = require('express');
 var router = express.Router();
 var sculptures_controller = require('../controllers/Sculptures');
 
-// Sculpture Routes
+// Routes for Sculptures
+
+// GET all sculptures (list)
 router.get('/', sculptures_controller.Sculptures_list);
+
+// POST a new sculpture (create)
 router.post('/', sculptures_controller.Sculptures_create_post);
+
+// GET a specific sculpture by ID (detail)
 router.get('/:id', sculptures_controller.Sculptures_detail);
+
+// PUT (update) a sculpture by ID
 router.put('/:id', sculptures_controller.Sculptures_update_put);
+
+// DELETE a sculpture by ID
 router.delete('/:id', sculptures_controller.Sculptures_delete);
 
 module.exports = router;
