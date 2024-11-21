087bc4031b5931400cd25170c3f9d1f6092c758c
VydehiR <vydehi1303@gmail.com>
2024-11-20 23:29:22 -0600

    changes made

--- app.js
+++ app.js
@@ -99,10 +99,9 @@ if (reseed) {
   recreateDB();
 }
 
-const port = process.env.PORT || 3000; // Use the port from .env or fallback to 3000
-
+const port = 3000;
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
 module.exports = app;
