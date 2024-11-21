7d799d3408309ed35bc1cca31d85f84f0d53095c
VydehiR <vydehi1303@gmail.com>
2024-11-20 19:39:45 -0600

    changes made

--- routes/Sculptures.js
+++ routes/Sculptures.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
 var sculptures_controller = require('../controllers/Sculptures');
 
@@ -20,4 +20,38 @@ router.put('/:id', sculptures_controller.Sculptures_update_put);
 router.delete('/:id', sculptures_controller.Sculptures_delete);
 
 
+module.exports = router;*/
+
+
+var express = require('express');
+var sculptures_controlers = require('../controllers/Sculptures');
+var router = express.Router();
+ 
+/* GET sculptures page. */
+//router.get('/', sculptures_controlers.Sculpture_view_all_Page);
+ 
+//router.get('/sculptures/:id', sculptures_controlers.sculptures_detail);
+router.get('/sculptures/:id', sculptures_controlers.Sculpture_detail);
+ 
+ 
+// POST route for creating a new sculptures
+router.post('/', sculptures_controlers.Sculptures_create_post);
+router.put('/:id', sculptures_controlers.Sculptures_update_put);
+ 
+router.delete('/:id', sculptures_controlers.Sculptures_delete);
+ 
+ 
+/* GET detail costume page */
+router.get('/detail', sculptures_controlers.Sculpture_view_one_Page);
+ 
+//router.get('/create', sculptures_controlers.Sculpture_create_Page);
+ 
+/* GET update costume page */
+//router.get('/update', sculptures_controlers.Sculpture_update_Page);
+ 
+/* GET delete costume page */
+//router.get('/delete', sculptures_controlers.sculpture_delete_Page);
+ 
+ 
 module.exports = router;
+ 
