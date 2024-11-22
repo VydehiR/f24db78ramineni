b3cae2aa1e7c66ca887a41bb2a420be0d35215a6
VydehiR <vydehi1303@gmail.com>
2024-11-22 09:59:46 -0600

    changes made

--- routes/Sculptures.js
+++ routes/Sculptures.js
@@ -2,22 +2,19 @@ var express = require('express');
 var router = express.Router();
 var sculpture_controller = require('../controllers/sculptures');
 
+// List all sculptures
 router.get('/', sculpture_controller.sculpture_list);
+router.post('/create', sculpture_controller.sculpture_create_post);
 
-router.post('/', sculpture_controller.sculpture_create_post);
-
-router.put('/sculptures:id', sculpture_controller.sculpture_update_put);
-
-router.delete('/sculptures:id', sculpture_controller.sculpture_delete);
-
-router.get('/sculptures:id', sculpture_controller.sculpture_detail);
-
-router.get('/detail', sculpture_controller.sculpture_view_one_Page);
+router.get('/:id', sculpture_controller.sculpture_detail);
+router.put('/:id', sculpture_controller.sculpture_update_put);
+router.delete('/:id', sculpture_controller.sculpture_delete);
 
 router.get('/create', sculpture_controller.sculpture_create_Page);
-
 router.get('/update', sculpture_controller.sculpture_update_Page);
-
 router.get('/delete', sculpture_controller.sculpture_delete_Page);
+router.get('/view/page', sculpture_controller.sculpture_view_all_Page);
+router.get('/view/:id', sculpture_controller.sculpture_view_one_Page);
+
 
 module.exports = router;
